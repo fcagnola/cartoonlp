f4a4a1b197c12b363b2972b546a0f32a95ab0fc2
fcagnola <fede.cagnola@gmail.com>
2021-09-12 19:04:58 +0200

    new functions

--- download_subs.py
+++ download_subs.py
@@ -4,6 +4,9 @@ from collections.abc import Mapping
 import json
 import os
 import pprint
+import pickle
+import random
+from typing import Tuple, List
 
 import requests
 from urllib.parse import quote_plus
@@ -13,12 +16,11 @@ BASE = "https://api.opensubtitles.com"
 SEARCH = "/api/v1/subtitles?type=movie&languages=en&ai_translated=exclude&hearing_impaired=exclude&query="
 
 # Authentication
-header = {"Api-Key": API_KEY}
 
 # Step 1: search for subs via API /api/v1/subtitles
 
-def get_list_tuples_titles():
-    with open("disney_titles.json", "r") as fp:
+def get_list_tuples_titles(path: str) -> List[ Tuple[str, str] ]:
+    with open(path, "r") as fp:
         titles = json.load(fp)
 
         # list of tuples (title, year)
@@ -26,27 +28,45 @@ def get_list_tuples_titles():
 
         return titles
 
-def get_download_link(title):
-    assert isinstance(title, tuple)
 
-    title, year = title
+def get_proxy() -> Tuple[str, str]:
+    PATH = 'proxies.pickle'
+    with open(PATH, "rb") as fp:
+        obj = pickle.load(fp)
+        proxy = random.choice(obj['proxies'])
+        return proxy
+        
+def get_subtitles():
+    titles: List = get_list_tuples_titles("disney_titles.json")
 
-    encoded_title = quote_plus(title)
-    uri = BASE+SEARCH+encoded_title+"&year="+str(year)
-    response = requests.get(uri, headers=header)
+    # Loop through titles
+    for title in titles:
+        # use a new IP address each time to avoid API limiting
+        ip, port = get_proxy()
+        proxies = {"https": "http://"+ip+":"+port}
+        # set the correct auth header
+        # header = {"Api-Key": API_KEY}
+        movie_title, year = title
+        encoded_title = quote_plus(movie_title)
+        uri = BASE+SEARCH+encoded_title+"&year="+str(year)
+        response = requests.get(uri, proxies=proxies)
 
-    if response.status_code >= 200:
+        if response.status_code == 200: # successful request
+            # Check if it behaves like a dict() through collections.abc abstract base class
+            print(f"Successful fetch: {movie_title}. [Proxy {ip}]")
+            add_sub_to_json(response.json())
 
-        # Check if it behaves like a dict() through collections.abc abstract base class
-        if isinstance(response.json(), Mapping):
-            pprint.pprint(response.json())
+        else:  # failed request
+            print(f"Failed fetch: {movie_title}. [Proxy {ip}]")
+            print(response.status_code, response.json())
 
-        elif isinstance(response.json(), str):
-            data = json.loads(response.json())
-            pprint.pprint(data)
 
-        else:
-            print(type(response.json()))
+def add_sub_to_json(json: str):
+    with open('opensubs_response.json', "a") as fp:
+        json.dump(json, fp)
+    
 
-    else:
-        print(response.status_code, response.json())
+
+
+
+get_subtitles()
