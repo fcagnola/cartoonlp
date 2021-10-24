e914692fa760f6769e8371b65b3c57e62f8e18e1
fcagnola <fede.cagnola@gmail.com>
2021-10-24 17:46:21 +0200

    cleanup

--- nltk_processing.py
+++ nltk_processing.py
@@ -10,11 +10,18 @@ with open("texts.pickle", "rb") as fp:
     data = pickle.load(fp)
 
 df = pd.DataFrame.from_dict(data, orient='index', columns=["Year", "Text"])
+# Dataframe: 
+# title - year - text
 
 def tokenize_and_remove_stopwords(txt):
+    """Basic tokenisation function with stopwords removal"""
     text = word_tokenize(txt)
     text = " ".join([word for word in text if word not in STOP_WORDS])
     return text
+
 df['tokenized'] = df['Text'].apply(lambda x: tokenize_and_remove_stopwords(x))
+# Dataframe: 
+# title - year - text - tokenized
+
 
 print(df.head(59))
